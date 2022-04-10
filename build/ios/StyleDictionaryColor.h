560f8432de1cb6f4bd9bfb998a5170b2d3775496
Katsu08 <Katsu08@users.noreply.github.com>
2022-04-10 21:28:46 +0000

    Tokens updated

--- build/ios/StyleDictionaryColor.h
+++ build/ios/StyleDictionaryColor.h
@@ -4,14 +4,14 @@
 //
 
 // Do not edit directly
-// Generated on Sun, 10 Apr 2022 20:48:59 GMT
+// Generated on Sun, 10 Apr 2022 21:28:46 GMT
 
 
 #import <UIKit/UIKit.h>
 
 typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
 ColorDanger200,
-ColorBg500,
+ColorOrange500,
 ColorBlue400,
 ColorBlue100,
 ColorBlue500
